208e54303d6677c92a459710c8723990d84362c5
Jallen Messersmith <jallenmessersmith@gmail.com>
2019-08-17 00:51:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,5 +52,15 @@ def reduce_to_all_true(source_array)
   return true 
 end
     
-  
+def reduce_to_any_true(source_array) 
+  i = 0 
+  while i < source_array.length do 
+    if source_array[i] == true 
+      return true
+    else 
+      i += 1 
+    end
+  end 
+  return false 
+end
 
